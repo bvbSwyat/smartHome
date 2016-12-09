71fefc387c1638e7177dff920b412e5ba6b4b445
swyat <swyatyxa@gmail.com>
2016-12-10 00:43:38 +0200

    fixed is sign_up

--- public/app/registration/registration.ctrl.js
+++ public/app/registration/registration.ctrl.js
@@ -1,10 +1,16 @@
 (function () {
-    angular.module("mainApp").controller("RegistrationController", function (RestApi, $state) {
+    angular.module("mainApp").controller("RegistrationController", function (RestApi, $state, UserFactory, $cookieStore, $rootScope) {
 
       var vm = this;
 
+        // remove it and newer use  -it must be session in next release
+      if ($cookieStore.get('isAuth')) {
+        $state.go($rootScope.$previousState);
+      }
+
       vm.signUp = function () {
         RestApi.signUp(vm.user).then(function (data) {
+           UserFactory.setUser(data.data);
            data.is_manager ? $state.go('user.usergroup') : $state.go('user.cardlist');
         }, function(error) {
             alert('You sent bad credentials!');
