791619e21071d8f449fcbd05fb7e8096bc462c42
Sviatoslav Boychuk <sboych@ch027.softservecom.com>
2016-12-09 11:29:46 +0200

    create some view

---
+++ public/ReadME.md
@@ -0,0 +1,50 @@
+## Installation
+
+Project requires:
+
+* [npm](https://www.digitalocean.com/community/tutorials/how-to-install-node-js-on-ubuntu-16-04)
+* [node](https://nodejs.org/uk/download/package-manager/) - >4.0
+
+### Node Instalation
+
+Check if installed Node.
+
+```sh
+$ node -v
+```
+ - If you will get different answer from this: "v...", node needs to be installed.
+
+```sh
+$ curl https://raw.githubusercontent.com/creationix/nvm/v0.25.0/install.sh | bash
+$ source ~/.profile
+$ nvm ls-remote
+$ nvm install v4.4.5
+$ nvm use v4.4.5
+```
+
+### NodeJS Instalation
+
+Check if installed Node.
+
+```sh
+$ nodejs -v
+```
+ - If you will get different answer from this: "v...", nodejs needs to be installed.
+
+```sh
+$ curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+$ sudo apt-get install -y nodejs
+$ sudo apt-get purge nodejs npm
+```
+
+### Global dependencies instalation
+
+Check if installed Node.
+
+```sh
+$ npm i gulp -g
+$ sudo apt-get install -y nodejs
+$ sudo apt-get purge nodejs npm
+```
+
+npm install --global gulp-cli
---
+++ public/gulpfile.js
@@ -0,0 +1,56 @@
+var gulp = require('gulp'),
+    webserver = require('gulp-webserver'),
+    browserSync = require('browser-sync').create(),
+    concat = require('gulp-concat'),
+    plumber = require('gulp-plumber'),
+    rename = require('gulp-rename'),
+    folders = require('gulp-folders'),
+    path = require('path'),
+    pathToFolder = 'app/modules';
+
+//init
+
+gulp.task('main-init', function () {
+    gulp.src([
+        'app/*.js',
+        'app/*/*.js'
+    ])
+        .pipe(plumber())
+        .pipe(concat('app.js'))
+        .pipe(plumber.stop())
+        .pipe(gulp.dest('dist/'));
+
+    gulp.src([
+        'app/*.html',
+        'app/*/*.html'
+    ]).pipe(gulp.dest('dist/templates/'));
+
+});
+
+gulp.task('style-init', function () {
+    gulp.src([
+        'style/*.css'
+    ]).pipe(gulp.dest('dist/style'));
+});
+
+gulp.task('init', ['main-init', 'style-init']);
+
+///serve, watch
+
+gulp.task('serve', ['watch'], function() {
+    gulp.src('.')
+        .pipe(webserver({
+            fallback: "index.html"
+        }));
+});
+
+gulp.task('watch', ['init'], function () {
+    browserSync.init({
+        server: "."
+    });
+
+    gulp.watch(['app/*', 'app/**/*', "style/*"], ['init']);
+    gulp.watch(['app/*', 'app/**/*', "style/*", "index.html"]).on('change', browserSync.reload);
+});
+
+gulp.task('default', ['serve']);
