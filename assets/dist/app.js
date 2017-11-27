12cbf1cf1c789644ae1ecaa27c62a0a3699c4103
swyat <swyatyxa@gmail.com>
2017-11-27 18:43:30 +0200

    adde mobile resolution

--- assets/dist/app.js
+++ assets/dist/app.js
@@ -259,6 +259,36 @@
 })();
 
 (function () {
+  angular.module("mainApp").controller("DashboardController", function (UserFactory, RestApi, $state) {
+    var vm = this;
+
+    vm.homeData = {
+      status: false,
+      temperature: null,
+      hSystem: {
+        status: false
+      },
+      kSystem: {
+        status: false
+      }
+    };
+    setInterval(function() {
+      RestApi.getHomeData().then(function(response) {
+        vm.homeData = response.data;
+      });
+    }, 3000);
+
+
+    vm.toggleSystem = function(systemName) {
+      vm.homeData[systemName].status = !this.homeData[systemName].status;
+      RestApi.setHomeData(this.homeData).then(function(response) {
+      });
+    };
+
+  })
+})();
+
+(function () {
   'use strict';
 
 
@@ -403,36 +433,6 @@
     }
   });
 }());
-
-(function () {
-  angular.module("mainApp").controller("DashboardController", function (UserFactory, RestApi, $state) {
-    var vm = this;
-
-    vm.homeData = {
-      status: false,
-      temperature: null,
-      hSystem: {
-        status: false
-      },
-      kSystem: {
-        status: false
-      }
-    };
-    setInterval(function() {
-      RestApi.getHomeData().then(function(response) {
-        vm.homeData = response.data;
-      });
-    }, 3000);
-
-
-    vm.toggleSystem = function(systemName) {
-      vm.homeData[systemName].status = !this.homeData[systemName].status;
-      RestApi.setHomeData(this.homeData).then(function(response) {
-      });
-    };
-
-  })
-})();
 
 (function () {
   angular.module("mainApp").controller("GroupsListController", function (RestApi, UserFactory, $uibModal) {
